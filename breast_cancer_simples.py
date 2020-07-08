d64f94cad9e2b095ec68e2ab68e0450970f0c26e
Gabriel Arruda <gabrielaugusto.arruda@gmail.com>
2020-07-07 22:42:12 -0300

    primeira execução e teste do modelo

--- breast_cancer_simples.py
+++ breast_cancer_simples.py
@@ -27,5 +27,26 @@ classificador.add(Dense(units= 16, activation='relu', kernel_initializer='random
 classificador.add(Dense(units=1, activation='sigmoid'))
 # activation='sigmoid' por ser um problema de classificação
 
+#compilar
+classificador.compile(optimizer='adam', loss='binary_crossentropy',
+                      metrics=['binary_accuracy'])
+#treino
+classificador.fit(x=x_train,y=y_train,batch_size=10,epochs=100)     
+# o output accuracy diz respeito a o teste aplicado na propria base de treino. desconsiderar
 
-print(classificador)
+#teste 
+predicted_data=classificador.predict(x=x_test)
+# o retorno desta função é  à probabilidade prevista para cada um dos registros, segundo o modelo
+# precisa-se comparar com os valores esperados no espaço amostral de teste (y_test). 
+#Para isso é necessario normalizar as probabiliaddes para as determinadas classes. Segue abaixo
+predicted_data= predicted_data>0.5
+
+# ========= Avaliação simples de performance do modelo =========
+#Com sklearn
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+accuracy=accuracy_score(y_true=y_test,y_pred=predicted_data)
+confusion_matrix=confusion_matrix(y_true=y_test,y_pred=predicted_data)
+#Com Keras
+evaluate=classificador.evaluate(x=x_test,y=y_test)
+
